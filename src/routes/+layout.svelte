b0b8de4eaa1ad42bb96583aec919731c9d412a26
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2023-05-09 12:37:16 -0700

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,52 @@
+<script lang='ts'>
+	// The ordering of these imports is critical to your app working properly
+	import '../theme.postcss';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/all.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+	import { AppShell, AppBar, Avatar, Drawer, drawerStore } from '@skeletonlabs/skeleton';
+	import Navigation from '$lib/components/navigation.svelte';
+
+	function drawerOpen(): void {
+		drawerStore.open();
+	}
+</script>
+
+<Drawer>
+	<Navigation />
+</Drawer>
+
+<AppShell slotSidebarLeft="w-0 md:w-52 bg-surface-500/10">
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<button class="md:hidden btn btn-sm mr-4">
+					<span>
+						<svg viewBox="0 0 100 80" class="fill-token w-4 h-4" on:click={drawerOpen} on:keypress={drawerOpen}>
+							<rect width="100" height="20" />
+							<rect y="30" width="100" height="20" />
+							<rect y="60" width="100" height="20" />
+						</svg>
+					</span>
+				</button>
+				<strong class="text-xl">Western Water</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<Avatar class="w-10" initials="WW" background="bg-primary-500" />
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<Navigation />
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment>
+	<svelte:fragment slot="pageHeader">Page Header</svelte:fragment>
+	<!-- Router Slot -->
+	<div class="container p-10 mx-auto">
+		<slot />
+	</div>
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<svelte:fragment slot="footer">Footer</svelte:fragment>
+</AppShell>
